13e9ee1a33dd359f817cab944ff51cb1a0d1b748
Jason Curran <jc@jasoncurran.com>
2016-02-03 20:35:42 -0500

    initital commit

---
+++ config/production.config.js
@@ -0,0 +1,19 @@
+var config = {
+	env: 'production',
+	api: {
+		base_url: 'http://apidemo.local/api',
+		defaultRequest: {
+			headers: {
+				'X-Requested-With': 'rest.js',
+				'Content-Type': 'application/json'
+			}
+		}
+	},
+	social: {
+		facebook: '',
+		twitter: '',
+		github: ''
+	},
+	debug: false,
+}
+module.exports = config;
